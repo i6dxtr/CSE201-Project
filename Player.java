7a0047a90320b168151cbb819816b41b63d2f047
Demetrius H <91034959+i6dxtr@users.noreply.github.com>
2024-11-03 14:31:31 -0500

    added documentation

--- Player.java
+++ Player.java
@@ -1,3 +1,8 @@
+/**
+ * Represents a player in the game with attributes like health, inventory, and equipped items,
+ * which can be controlled by the user through the CLI.
+ */
+
 public class Player {
     private String name;
     private int maxHealth;
@@ -7,6 +12,16 @@ public class Player {
     private Room currentRoom;
     private boolean isDefending;
 
+    /**
+     * Creates a new Player with the specified name.
+     * Initializes the player with default values:
+     * - Maximum health set to 100
+     * - Starting health set to 100
+     * - Uses basic sword for starting weapon
+     * - Inventory with only basic sword
+     * 
+     * @param name is the user-given name of the player
+     */
     public Player(String name) {
         this.name = name;
         this.maxHealth = 100;
@@ -17,23 +32,48 @@ public class Player {
         this.isDefending = false;
     }
 
+
+    /**
+     * Moves the player to a new room and displays a message to the terminal.
+     * 
+     * @param room is the next room the player chooses to move to
+     */
     public void moveTo(Room room) {
         this.currentRoom = room;
         System.out.println("You move to " + room.getName());
     }
 
+    /**
+     * Outputs the current health status of the player to console.
+     * Displayed in the format: "Health: current/max"
+     */
     public void displayHealth() {
         System.out.println("Health: " + health + "/" + maxHealth);
     }
 
+    /**
+     * Gets the current room where the player is located.
+     * 
+     * @return the current room
+     */
     public Room getCurrentRoom() {
         return currentRoom;
     }
 
+    /**
+     * Sets the current room of the player.
+     * 
+     * @param room the room to set the location to
+     */
     public void setCurrentRoom(Room room) {
         this.currentRoom = room;
     }
 
+    /**
+     * Gets the name of the player.
+     * 
+     * @return the player's name
+     */
     public String getName() {
         return name;
     }
