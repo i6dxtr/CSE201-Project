d0d16fd6ebffa973c60b592a5f36822745fa8ca9
tom2433 <tom.2433.england@gmail.com>
2024-11-03 12:53:46 -0600

    Requirement javadoc

--- Requirement.java
+++ Requirement.java
@@ -1,14 +1,37 @@
+/**
+ * Class: Requirement
+ * @author Thomas England
+ * @version 1.0
+ * Course : CSE 201 Fall 2024
+ * Written: October 28th, 2024
+ *
+ * Purpose: This class creates requirements that the user must meet before entering a room.
+ */
 public class Requirement {
+    // String to store requirement description
     private String description;
+    // boolean to store status of whether requirement has been met
     private boolean isMet;
 
+    /**
+     * Constructs a requirement with a specified description.
+     *
+     * @param description A string value containing the requirement description
+     */
     public Requirement(String description) {
+        // every requirement starts off as not met
         this.description = description;
         this.isMet = false;
     }
 
+    /**
+     * Checks the status of whether the specified player has met the requirement.
+     *
+     * @param player The specified player to check requirement status.
+     * @return A boolean value indicating true if the requirement has been met or false otherwise.
+     */
     public boolean check(Player player) {
-        // Simplified for now
+        // simplified for now
         return isMet;
     }
 }
