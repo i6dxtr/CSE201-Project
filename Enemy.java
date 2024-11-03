01459d125f8c6f8a94034c26e9b2ebb043a2b5a0
mistergreenjeans <60331417+mistergreenjeans@users.noreply.github.com>
2024-11-03 13:47:09 -0500

    Javadoc for Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,3 +1,12 @@
+/**
+* Class: Enemy
+* @author Josh
+* @version 1.0
+* Course : CSE 201 Fall 2024
+* Written: October 30, 2024
+*
+* Purpose: – Class to store enemy Information and methods
+*/
 public abstract class Enemy {
     protected String name;
     protected String description;
@@ -5,6 +14,13 @@ public abstract class Enemy {
     protected int maxHealth;
     protected int attackPower;
 
+    /**
+     * Enemy contrstuctor
+     * @param name : name of the enemy
+     * @param description : description of the enemy
+     * @param health : health of the enemy
+     * @param attackPower : health of the enemy
+     */
     public Enemy(String name, String description, int health, int attackPower){
         this.name = name;
         this.description = description;
@@ -13,14 +29,26 @@ public abstract class Enemy {
         this.attackPower = attackPower;
     }
 
+    /**
+     * Method to get the name of the enemy
+     * @return name of the enemy
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Method to get the flee chance of the enemy
+     * @return the enemys flee chance
+     */
     public double getFleeChance() {
         return 0.5; // Or some default implementation
     }
 
+    /**
+     * Method to get the enemies action (what they decide to do)
+     * @return a string with the action the enemy is preparing
+     */
     public String getIntendedAction() {
         return "Enemy is preparing an attack.";
     }
