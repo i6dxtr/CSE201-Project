fae0e5e35df1cc002373ed1dc93b94104cfd41eb
joshrosenblatt123 <rosenbjz@miamioh.edu>
2024-11-03 13:35:03 -0500

    Modified functionality of some classes.

--- Main.java
+++ Main.java
@@ -1,12 +1,30 @@
 import java.util.Scanner;
 
+/**
+ * Class: Main
+ * Entry point for the text-based adventure game. Prompts the user to input a name,
+ * initializes the game with that name, and starts the game loop.
+ *
+ * @author Joshua Rosenblatt
+ * @version 1.0
+ * Course: CSE 201 Fall 2024
+ * Written: November 1, 2024
+ */
 public class Main {
+
+    /**
+     * Main method that serves as the entry point for the game.
+     * Prompts the user for a player name, initializes the game, and starts it.
+     *
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
-        Game game = new Game();
+        Game game = new Game(); // Create a new game instance
         Scanner sc = new Scanner(System.in);
         System.out.println("Please input a name.");
         String name = sc.nextLine();
-        game.initializeGame(name);
+
+        game.initializeGame(name); // Initialize the game with the provided name
         game.startGame();
     }
 }
--- Potion.java
+++ Potion.java
@@ -1,14 +1,32 @@
+/**
+ * Class: Potion
+ * Represents a potion item that can be used by a player to heal or cause damage,
+ * with a certain chance of success. Potions can only be used once.
+ *
+ * Extends the Item class.
+ *
+ * @author Joshua Rosenblatt
+ * @version 1.0
+ * Course: CSE 201 Fall 2024
+ * Written: November 3, 2024
+ */
 public class Potion extends Item {
 
-    int healAmount;
-    int damageAmount;
-    double chance;
+    int healAmount;       // Amount of healing the potion provides
+    int damageAmount;     // Amount of damage the potion causes
+    double chance;        // Chance of the potion working
+    boolean isConsumed;   // Indicates if the potion has been used
 
-    boolean isConsumed;
-
-
-
-    Potion(String name, String description, int healAmount, int damageAmount, double chance){
+    /**
+     * Constructs a Potion with specified attributes.
+     *
+     * @param name        The name of the potion.
+     * @param description A description of the potion.
+     * @param healAmount  The amount of healing the potion provides.
+     * @param damageAmount The amount of damage the potion causes.
+     * @param chance      The chance that the potion will be effective.
+     */
+    Potion(String name, String description, int healAmount, int damageAmount, double chance) {
         super(name, description);
         this.healAmount = healAmount;
         this.damageAmount = damageAmount;
@@ -16,22 +34,25 @@ public class Potion extends Item {
         isConsumed = false;
     }
 
-    // I'm having alot of fun with these default constructors,
-    // its gonna be really sad when they have to be replaced
-    Potion(){
+    /**
+     * Constructs a default Potion with preset attributes.
+     */
+    Potion() {
         super("Red Potion", "Only for those who are worthy");
         healAmount = Integer.MIN_VALUE;
         damageAmount = 0;
-        chance = 1;
+        chance = 1.0;
         isConsumed = false;
     }
 
-    public void use(Player player){
-        if(!isConsumed){
-            //player.heal(healAmount);
-            // or
-            //player.health += healAmount;
-            //player.health = min(player.maxHealth, player.health);
+    /**
+     * Uses the potion on a player, applying its effects if it has not been consumed.
+     *
+     * @param player The player who uses the potion.
+     */
+    public void use(Player player) {
+        if (!isConsumed) {
+            // Apply effects to player, healing or damage, based on potion attributes
             isConsumed = true;
         }
     }
